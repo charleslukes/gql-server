ed07d62e3c9632b8370a1b600924b57437e54156
charleslukes <charleslukes28@gmail.com>
2019-12-27 19:19:42 +0100

    add schema for the author

--- src/server.js
+++ src/server.js
@@ -1,8 +1,37 @@
 const express = require("express");
 const expressGraphql = require("express-graphql");
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLInt
+} = require("graphql");
+const { authors, books } = require("./data");
 
+// initiates express
 const app = express();
 
+// schema for the author type
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  description: "This describes the author properties",
+  fields: () => ({
+    id: {
+      type: GraphQLNonNull(GraphQLInt)
+    },
+    name: {
+      type: GraphQLNonNull(GraphQLString)
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve: parent => {
+        return books.filter(elem => elem.authorId === parent.id);
+      }
+    }
+  })
+});
 app.use(
   "/graphql",
   expressGraphql({
